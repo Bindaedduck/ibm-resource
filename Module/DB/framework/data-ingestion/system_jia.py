2e2ef9ce4cd24c4dd57c3cadffc5970678b58f89
Bindaedduck <seokjin.jang@kstec.co.kr>
2026-05-27 20:56:50 +0900

    system_jia.py 업데이트

--- Module/DB/framework/data-ingestion/system_jia.py
+++ Module/DB/framework/data-ingestion/system_jia.py
@@ -6,8 +6,32 @@ from.base import BaseIngestion
 
 class SystemJIRAIngestion(BaseIngestion):
 
-    def ingestion_logic(self):
-        return
+    def _read_file(self, chunk_size=100000):
+        try:
+        	for zip_file in self.zip_file_list:
+                with zipfile.ZipFile(zip_file, 'r') as z:
+                	json_file_list = [m for m in z.namelist() if m.endswith('.json')]
+                    
+                    for json_file in json_file_list:
+                        self.logger.info(f"[*] {json_file} 파일 작업 시작")
+                        with z.open(json_file, 'r') as f:
+                        	records = ijson.items(f, 'item')
+                            chunk = []
+                            
+                            for i, record in enumerate(records):
+                                chunk.append(record)
+                                
+                                if(i+1) % chunk_size == 0 :
+                                    yield chunk
+                                    chunk = []
+                            
+                            if chunk:
+                                yield chunk
+                            
+        exception Exception as e:
+        	self.logger.error(f"[-] Error: {str(e)", exc_info=True}
 
-    def col_sync_logic(self):
+    def _extract(self, data):
         return
+    
+    def _transform(self, data, file_index=None):
